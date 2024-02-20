2d98c0deacf77197d9559bb11e06eb55216ef269
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-02-20 15:45:37 +0000

    - api name update - adding aws path origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'https://main.d37fcxrvcw86hr.amplifyapp.com'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:5174', 'https://main.d23ylsy8h1d6lf.amplifyapp.com'],
 
     'allowed_origins_patterns' => [],
 
