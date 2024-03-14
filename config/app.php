ddb1dcd1a1863657532c6edbfdd39ce76f1889c4
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-14 05:54:19 +0000

    - small changes

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://malvern-api-production.up.railway.app/'),
+    'url' => env('APP_URL', 'https://malvern-api-production.up.railway.app'),
 
     'asset_url' => env('ASSET_URL'),
 
