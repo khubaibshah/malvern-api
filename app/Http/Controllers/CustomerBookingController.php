d778eff075bf4b6287edb3ac01a1ad67dcc09e53
Khubaib Ul Haque <khubaib_12@hotmail.com>
2024-03-14 01:36:06 +0000

    - changing route for create booking

--- app/Http/Controllers/CustomerBookingController.php
+++ app/Http/Controllers/CustomerBookingController.php
@@ -18,19 +18,19 @@ class CustomerBookingController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'Booking_refrence' => 'required|string',
-            'vehicle_make' => 'required|string',
-            'vehicle_model' => 'required|string',
-            'job_repair_id' => 'required|numeric',
-            'bookings_datetime' => 'required|date',
-            'customer_notes' => 'nullable|string',
-            'deposit_paid' => 'required|numeric',
-            'repair_price' => 'required|numeric',
+            'Booking_reference' => 'required|numeric|max:255',
+            'vehicle_make' => 'required|string|max:255',
+            'vehicle_model' => 'required|string|max:255',
+            'job_repair_id' => 'required|numeric|max:255',
+            'bookings_datetime' => 'required|date|max:255',
+            'customer_notes' => 'nullable|string|max:255',
+            'deposit_paid' => 'required|numeric|max:255',
+            'repair_price' => 'required|numeric|max:255',
         ]);
 
         // Create a new customer booking
         $customerBooking = CustomerBooking::create($request->all());
 
-        return response()->json(['message' => 'Booking created successfully', 'data' => $customerBooking], 201);
+        return response()->json($customerBooking, 201);
     }
 }
