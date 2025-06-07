b3f2a70033e83a2d3b0b74b17411e3aca4ececfb
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-06-07 11:33:40 +0100

    update to migration

--- database/migrations/2025_06_06_153958_add_engine_size_to_scs_cars_table.php
+++ database/migrations/2025_06_06_153958_add_engine_size_to_scs_cars_table.php
@@ -8,15 +8,19 @@ class AddEngineSizeToScsCarsTable extends Migration
 {
     public function up()
     {
-        Schema::table('scs_cars', function (Blueprint $table) {
-            $table->string('engine_size', 255)->nullable()->after('keys'); // store as string
-        });
+        if (!Schema::hasColumn('scs_cars', 'engine_size')) {
+            Schema::table('scs_cars', function (Blueprint $table) {
+                $table->string('engine_size', 255)->nullable()->after('keys');
+            });
+        }
     }
 
     public function down()
     {
-        Schema::table('scs_cars', function (Blueprint $table) {
-            $table->dropColumn('engine_size');
-        });
+        if (Schema::hasColumn('scs_cars', 'engine_size')) {
+            Schema::table('scs_cars', function (Blueprint $table) {
+                $table->dropColumn('engine_size');
+            });
+        }
     }
 }
