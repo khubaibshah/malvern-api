1476e4fcaedadebe8f0d7683d6129c42c2ff2905
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-06-09 13:45:19 +0100

    updating fillable columns for mass update

--- app/Models/ScsCar.php
+++ app/Models/ScsCar.php
@@ -8,13 +8,15 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ScsCar extends Model
 {
+    protected $table = 'scs_cars';
+
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'make', 'model', 'year', 'vrm', 'reg_date', 'registration_date','man_year',
         'variant', 'price', 'plus_vat', 'vat_qualifying', 'was_price', 'trade_price',
         'trade_text', 'price_above_40k', 'mileage', 'engine_cc', 'fuel_type',
-        'body_style', 'colour', 'doors', 'gearbox', 'keys', 'veh_type', 'description', 'registration'
+        'body_style', 'colour', 'doors', 'gearbox', 'keys', 'veh_type', 'description', 'registration', 'deleted_at'
     ];
     
     public function images()
