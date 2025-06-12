ba8e9ebf3365e442fa8ae7fbd460b4a9ffc8b1b9
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-06-12 18:38:14 +0100

    parsing more data from hpi report

--- app/Services/HpiReportParserService.php
+++ app/Services/HpiReportParserService.php
@@ -12,25 +12,33 @@ class HpiReportParserService
         $pdf = $parser->parseFile($filePath);
         $text = $pdf->getText();
 
+        //get the raw file and save to logs
+        // file_put_contents(storage_path('logs/raw_pdf.txt'), $text);
+
+
         return [
-            'registration'   => $this->extractBetween($text, 'VRM', 'VIN'),
-            'vin'            => $this->extractBetween($text, 'VIN', 'Engine number'),
-            'engine_number'  => $this->extractBetween($text, 'Engine number', 'Colour'),
-            'make'           => $this->extractBetween($text, 'Make', 'Model'),
-            'model'          => $this->extractBetween($text, 'Model', 'Body type'),
-            'mot_expiry'     => $this->extractBetween($text, 'MOT expiry', 'Mileage'),
-            'mileage'        => $this->extractBetween($text, 'Mileage recording', 'ADVISORY'),
-            'advisories'     => $this->extractBetween($text, 'ADVISORY NOTICES', 'HISTORIC TESTS'),
-        ];
+        'registration'   => $this->extractBetween($text, 'VRM', 'VIN'),
+        'vin'            => $this->extractBetween($text, 'VIN', 'Engine number'),
+        'engine_number'  => $this->extractBetween($text, 'Engine number', 'Colour'),
+        'make'           => $this->extractBetween($text, 'Make', 'Model'),
+        'model'          => $this->extractBetween($text, 'Model', 'Body type'),
+        'body_type'      => $this->extractBetween($text, 'Body type', 'Fuel type'),
+        'transmission'   => $this->extractBetween($text, 'Transmission', 'Engine capacity'),
+        'current_v5c_issue_date' => $this->extractBetween($text, 'Current V5C issue date', 'CO2 emissions'),
+        'mileage'        => $this->extractBetween($text, 'Mileage recording', 'ADVISORY'),
+        'advisories'     => $this->extractBetween($text, 'ADVISORY NOTICES', 'HISTORIC TESTS'),
+        'previous_owners' => $this->extractBetween($text, 'Number of previous owners', 'Current V5C issue date'),
+    ];
     }
 
     private function extractBetween(string $text, string $start, string $end): string
     {
-        $pattern = '/' . preg_quote($start, '/') . '(.*?)' . preg_quote($end, '/') . '/s';
+        $pattern = '/' . preg_quote($start, '/') . '\s*(.*?)\s*' . preg_quote($end, '/') . '/si';
         if (preg_match($pattern, $text, $matches)) {
             return trim($matches[1]);
         }
 
         return 'Not found';
     }
+
 }
