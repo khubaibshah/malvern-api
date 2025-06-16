6aea78ed7394dfe57fbd24a51dcba87e83721a0d
Khubaib Ul Haque <khubaib_12@hotmail.com>
2025-06-16 17:40:57 +0100

    feat: update application name to SCS and enhance email template for "Sell Your Car" submissions with improved formatting and additional vehicle details

--- resources/views/emails/sell_your_car.blade.php
+++ resources/views/emails/sell_your_car.blade.php
@@ -1,20 +1,31 @@
 @component('mail::message')
-# New "Sell Your Car" Submission
-
-**Name:** {{ $data->full_name }}  
-**Email:** {{ $data->email }}  
-**Phone:** {{ $data->phone ?? 'N/A' }}  
-**Postcode:** {{ $data->postcode }}  
-**Registration:** {{ $data->vehicle->registration ?? 'N/A' }}
+# 🚗 New "Sell Your Car" Submission
 
 ---
 
-## Vehicle Details
-**Make:** {{ $data->vehicle->make ?? 'N/A' }}  
-**Model:** {{ $data->vehicle->model ?? 'N/A' }}  
-**Colour:** {{ $data->vehicle->primary_colour ?? 'N/A' }}  
-**Fuel Type:** {{ $data->vehicle->fuel_type ?? 'N/A' }}  
-**Engine Size:** {{ $data->vehicle->engine_size ?? 'N/A' }}  
-**Mileage:** {{ $data->vehicle->odometer_value ?? 'N/A' }} {{ $data->vehicle->odometer_unit ?? '' }}
+## 📋 Customer Details
 
+- **Full Name:** {{ $data->full_name }}
+- **Email:** {{ $data->email }}
+- **Phone:** {{ $data->phone ?? 'N/A' }}
+- **Postcode:** {{ $data->postcode }}
+- **Registration:** {{ $data->vehicle->registration ?? 'N/A' }}
+
+---
+
+## 🚘 Vehicle Information
+
+- **Make:** {{ $data->vehicle->make ?? 'N/A' }}
+- **Model:** {{ $data->vehicle->model ?? 'N/A' }}
+- **Colour:** {{ $data->vehicle->primary_colour ?? 'N/A' }}
+- **Fuel Type:** {{ $data->vehicle->fuel_type ?? 'N/A' }}
+- **Engine Size:** {{ $data->vehicle->engine_size ?? 'N/A' }}
+- **Mileage:** {{ $data->vehicle->odometer_value ?? 'N/A' }} {{ $data->vehicle->odometer_unit ?? '' }}
+- **First Used:** {{ $data->vehicle->first_used_date ?? 'N/A' }}
+- **Registration Date:** {{ $data->vehicle->registration_date ?? 'N/A' }}
+
+---
+
+Thanks,  
+**{{ config('app.name') }} Sales Team**
 @endcomponent
